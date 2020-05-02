7842f762802832018a3c2a2edc7fcf2a3f4271c7
Jeffrey-Suitor <jeffreysuitor117@gmail.com>
2020-05-02 18:34:48 -0400

    Trying To Get Radar Working 2: Electric Boogaloo

--- ReactNativeMaps/android/app/src/main/java/com/reactnativemaps/MainApplication.java
+++ ReactNativeMaps/android/app/src/main/java/com/reactnativemaps/MainApplication.java
@@ -12,6 +12,8 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import io.radar.sdk.Radar;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -47,7 +49,6 @@ public class MainApplication extends Application implements ReactApplication {
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     Radar.initialize(this, "prj_live_pk_09cb48731d4dc21b14ad71ad6f8499619d6200b2");
-
   }
 
   /**
